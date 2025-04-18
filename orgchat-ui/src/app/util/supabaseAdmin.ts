fd06b8454c5c714aa8136e1176dcfb4028f87191
goravaa <165974428+goravaa@users.noreply.github.com>
2025-04-19 03:14:01 +0530

    rebuilding UI

--- orgchat-ui/src/app/util/supabaseAdmin.ts
+++
@@ -1,9 +0,0 @@
-// lib/supabaseAdmin.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // only use on server
-)
-
-export default supabaseAdmin
